666d68a39f0edf7db9c3e1dba32787aa05e5fc77
Arnab Jana <arnabjana0099@gmail.com>
2025-08-16 21:01:20 +0530

    try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
   plugins: [react(), tailwindcss()],
 });
